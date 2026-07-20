ebcef76aa619bf9e0aaf9dd92c55197204060323
Yuvika1831 <157943194+Yuvika1831@users.noreply.github.com>
2026-07-20 09:43:22 +0530

    Implement longest consecutive sequence algorithm

---
+++ LongestConsecutiveSequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0) return 0;
+        Arrays.sort(nums);
+        int cur =1;
+        int len =1;
+        
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                continue;
+            }
+            else if(nums[i]==nums[i-1]+1){
+                len++;
+                
+
+            }
+            else {
+                len=1;
+            }
+           cur = Math.max(cur,len);
+        }
+        return cur;
+    }
+}
